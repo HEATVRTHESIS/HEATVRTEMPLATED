16e36318d5fac116e774be244132a80c782ee8a4
LenKaShen <hackbaita@gmail.com>
2025-10-02 20:14:02 +0800

    added hands

--- Assets/Scripts/ToggleObject.cs
+++ Assets/Scripts/ToggleObject.cs
@@ -3,24 +3,33 @@ using UnityEngine.InputSystem;
 
 public class ToggleObject : MonoBehaviour
 {
-    // Reference to the Input Action you created
-    public InputActionReference toggleAction;
-
     // Reference to the GameObject you want to toggle
     public GameObject objectToToggle;
+
+    // Input action for the left controller X button
+    private InputAction toggleAction;
+
+    private void Awake()
+    {
+        // Create an input action for the left controller X button (secondary button)
+        toggleAction = new InputAction(
+            name: "Toggle",
+            binding: "<XRController>{LeftHand}/secondaryButton"
+        );
+    }
 
     // Subscribe to the action when the script is enabled
     private void OnEnable()
     {
-        toggleAction.action.Enable();
-        toggleAction.action.performed += ToggleVisibility;
+        toggleAction.Enable();
+        toggleAction.performed += ToggleVisibility;
     }
 
     // Unsubscribe from the action when the script is disabled
     private void OnDisable()
     {
-        toggleAction.action.performed -= ToggleVisibility;
-        toggleAction.action.Disable();
+        toggleAction.performed -= ToggleVisibility;
+        toggleAction.Disable();
     }
 
     // This function is called every time the action is performed
