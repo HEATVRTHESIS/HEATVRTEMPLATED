dcf167024330c09f5386bdf73f82eda7e8f59fec
LenKaShen <hackbaita@gmail.com>
2025-10-07 14:10:09 +0800

    fix interaction toolkit

--- Assets/VR Hands/HandScripts/HandAnimation.cs
+++ Assets/VR Hands/HandScripts/HandAnimation.cs
@@ -8,18 +8,46 @@ public class HandAnimation : MonoBehaviour
     [SerializeField] private InputActionReference pinchAction;
     private Animator animator;
 
-    private void OnEnable()
+    private void Awake() 
     {
-        //grip
-        gripAction.action.performed += Gripping;
-        gripAction.action.canceled += GripRelease;
-
-        //pinch
-        pinchAction.action.performed += Pinching;
-        pinchAction.action.canceled += PinchRelease;
+        animator = GetComponent<Animator>();
     }
 
-    private void Awake() => animator = GetComponent<Animator>();
+    private void OnEnable()
+    {
+        // Enable actions before subscribing
+        if (gripAction != null && gripAction.action != null)
+        {
+            gripAction.action.Enable();
+            gripAction.action.performed += Gripping;
+            gripAction.action.canceled += GripRelease;
+        }
+
+        if (pinchAction != null && pinchAction.action != null)
+        {
+            pinchAction.action.Enable();
+            pinchAction.action.performed += Pinching;
+            pinchAction.action.canceled += PinchRelease;
+        }
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe and disable actions
+        if (gripAction != null && gripAction.action != null)
+        {
+            gripAction.action.performed -= Gripping;
+            gripAction.action.canceled -= GripRelease;
+            gripAction.action.Disable();
+        }
+
+        if (pinchAction != null && pinchAction.action != null)
+        {
+            pinchAction.action.performed -= Pinching;
+            pinchAction.action.canceled -= PinchRelease;
+            pinchAction.action.Disable();
+        }
+    }
 
     private void Gripping(InputAction.CallbackContext obj) => animator.SetFloat("Grip", obj.ReadValue<float>());
 
@@ -28,5 +56,4 @@ public class HandAnimation : MonoBehaviour
     private void Pinching(InputAction.CallbackContext obj) => animator.SetFloat("Pinch", obj.ReadValue<float>());
 
     private void PinchRelease(InputAction.CallbackContext obj) => animator.SetFloat("Pinch", 0f);
-
 }
